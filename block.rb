bfdcc2bfce8b1e3011e38858fe2cadd339cb5b74
Francisco Javier Gutiérrez Gallego <114072492+Franguti4k@users.noreply.github.com>
2025-02-27 19:40:29 +0100

    Update block.rb

--- block.rb
+++ block.rb
@@ -1,18 +1,19 @@
 class Block
   attr_reader :index, :timestamp, :transactions, 
 							:transactions_count, :previous_hash, 
-							:nonce, :hash 
+							:nonce, :hash, :creator 
 
-  def initialize(index, transactions, previous_hash)
+  def initialize(index, timestamp, transactions, previous_hash, creator)
     @index         		 	 = index
     @timestamp      	 	 = Time.now
     @transactions 	 		 = transactions
 		@transactions_count  = transactions.size
     @previous_hash 		 	 = previous_hash
+    @creator             = creator
     @nonce, @hash  		 	 = compute_hash_with_proof_of_work
   end
 
-	def compute_hash_with_proof_of_work(difficulty="00")
+	def compute_hash_with_proof_of_work(difficulty="00000")
 		nonce = 0
 		loop do 
 			hash = calc_hash_with_nonce(nonce)
@@ -37,10 +38,10 @@ class Block
 
   def self.first( *transactions )    # Create genesis block
     ## Uses index zero (0) and arbitrary previous_hash ("0")
-    Block.new( 0, transactions, "0" )
+      Block.new(0, Time.now, "Genesis Block", "0", "Elam")
   end
 
   def self.next( previous, transactions )
-    Block.new( previous.index+1, transactions, previous.hash )
+    Block.new( previous.index+1,Time.now, transactions, previous.hash, "Francisco" )
   end
 end  # class Block
